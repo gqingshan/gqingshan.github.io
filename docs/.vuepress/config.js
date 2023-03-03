1796801ad126aa5aaca0254963d621981592d02b
gqingshan <root@localhost.localdomain>
2023-03-03 18:56:42 +0800

    first commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,4 +2,12 @@ module.exports = {
   title: 'gqingshan',
   description: '个人博客',
   base: '/'  // 部署站点的基础路径
+  themeConfig: {
+    logo: '',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'CSDN', link: 'https://www.csdn.net/' },
+      { text: 'GitHub', link: 'https://github.com/gqingshan'},
+    ]
+  }
 }
